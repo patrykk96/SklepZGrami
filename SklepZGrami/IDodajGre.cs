248999fe975b6e6bdca134c7b04ed1c70630aec8
Patryk Kucinski <rambotrix@gmail.com>
2017-01-14 16:43:43 +0100

    Dodanie interfejsu IDodajGre

---
+++ SklepZGrami/IDodajGre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepZGrami
+{
+    interface IDodajGre
+    {
+        void DodajGreWideo(string nazwa, double cena, string gatunek, string dataPremiery, int ograniczenieWiekowe, string platforma);
+        void DodajGrePlanszowa(string nazwa, double cena, string gatunek, string dataPremiery, int ograniczenieWiekowe, int minimumOsob, int maximumOsob, double waga);
+    }
+}
