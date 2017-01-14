a91a5db9422b97f0f5b79194f1661c7ebef59ddb
Patryk Kucinski <rambotrix@gmail.com>
2017-01-14 14:55:16 +0100

    Stworzenie klasy Gra

---
+++ SklepZGrami/gra.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepZGrami
+{
+    class Gra
+    {
+        protected string nazwa;
+        protected int cena;
+        protected string producent;
+        protected string wydawca;
+        protected string gatunek;
+        protected string dataPremiery;
+
+        public Gra() { }
+
+        public Gra(string nazwa, int cena, string producent, string wydawca, string gatunek, string dataPremiery)
+        {
+            this.nazwa = nazwa;
+            this.cena = cena;
+            this.producent = producent;
+            this.wydawca = wydawca;
+            this.gatunek = gatunek;
+            this.dataPremiery = dataPremiery;
+        }
+
+    }
+}
